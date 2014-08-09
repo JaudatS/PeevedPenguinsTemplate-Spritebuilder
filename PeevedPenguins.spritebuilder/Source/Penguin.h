40ca8157e048f0598a498aac8d6bac867d680c0c
Jaudat Syed <sjh.jaudat@gmail.com>
2014-08-08 22:31:31 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Jaudat Hussain Syed on 2014-08-08.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Jaudat Hussain Syed on 2014-08-08.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
